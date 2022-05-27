6c64aeff081d0a39b62987a3cf0af20b46e7ebf4
Soruso <101566665+Soruso@users.noreply.github.com>
2022-05-27 23:57:36 +0300

    Add files via upload

---
+++ Burov_Sergei_DZ2_Zadanie1.py
@@ -0,0 +1,8 @@
+first_digit = 15 * 3
+second_digit = 15 / 3
+third_digit = 15 // 2
+four_digit = 15 ** 2
+print(type(first_digit))
+print(type(second_digit))
+print(type(third_digit))
+print(type(four_digit))
