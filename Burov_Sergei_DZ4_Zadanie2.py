1c5348e7b2ea893fa6f489030416ee817e70df8a
Soruso <101566665+Soruso@users.noreply.github.com>
2022-06-07 00:28:16 +0300

    Add files via upload

---
+++ Burov_Sergei_DZ4_Zadanie2.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import pprint
+
+
+def currency_rates():
+    valuta = input('Введите валюту')
+    valuta = valuta.upper()
+    URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+
+    response = requests.get(URL)
+
+    dict_json = json.loads(response.text)
+    kurs = dict_json['Valute'][valuta]['Value']
+    print(f'1 {valuta} =  {kurs} RUB')
+    return
+currency_rates()
