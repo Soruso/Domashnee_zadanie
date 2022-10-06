e00df7079e5ee3991a0c67b5fee3cb899dd645cf
Soruso <burov.1990@gmail.com>
2022-10-06 15:23:09 +0300

    test1

--- Burov_Sergei_DZ2_Zadanie5.py
+++ Burov_Sergei_DZ2_Zadanie5.py
@@ -1,5 +1,7 @@
 lst = [57.8, 46.51, 97, 23.56, 75.22, 90.01, 76.6, 37.80, 50.21, 1.90]
 lst.sort()
+
+
 def output():
     for i in lst:
         num = int(i)
@@ -13,6 +15,8 @@ def output():
             print(f'"0{num} руб {num_2} коп"', end=" ")
         else:
             print(f'"{num} руб {num_2} коп"', end=" ")
+
+
 output()
 lst.reverse()
 output()
